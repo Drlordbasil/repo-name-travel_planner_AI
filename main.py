4c79d0461cf498b17c3a598d50dc07071379702d
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-25 21:28:15 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,115 @@
+Here's an improved version of your code:
+
+```python
+import datetime
+import random
+
+class UserProfile:
+    def __init__(self, preferences, interests, budget, travel_dates, requirements):
+        self.preferences = preferences
+        self.interests = interests
+        self.budget = budget
+        self.travel_dates = travel_dates
+        self.requirements = requirements
+
+
+class Destination:
+    def __init__(self, name, attractions, events, activities, local_experiences):
+        self.name = name
+        self.attractions = attractions
+        self.events = events
+        self.activities = activities
+        self.local_experiences = local_experiences
+
+
+class Itinerary:
+    def __init__(self, destination, days):
+        self.destination = destination
+        self.days = days
+
+
+class AIPlanner:
+    def __init__(self):
+        self.destinations = []
+        self.user_profile = None
+        self.itinerary = None
+
+    def create_user_profile(self, preferences, interests, budget, travel_dates, requirements):
+        self.user_profile = UserProfile(preferences, interests, budget, travel_dates, requirements)
+
+    def add_destination(self, destination):
+        self.destinations.append(destination)
+
+    def generate_itinerary(self):
+        if self.user_profile and self.destinations:
+            days = self.calculate_travel_days()
+            random_destination = random.choice(self.destinations)
+            self.itinerary = Itinerary(random_destination, days)
+            self.optimize_itinerary()
+        else:
+            print("Please create user profile and add destinations before generating the itinerary.")
+
+    def calculate_travel_days(self):
+        start_date = datetime.datetime.strptime(self.user_profile.travel_dates[0], "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(self.user_profile.travel_dates[1], "%Y-%m-%d")
+        days = (end_date - start_date).days
+        return days
+
+    def optimize_itinerary(self):
+        # Implement machine learning algorithms for itinerary optimization
+        pass
+
+    def display_itinerary(self):
+        if self.itinerary:
+            print(f"Destination: {self.itinerary.destination.name}")
+            print("Itinerary:")
+            for day in range(1, self.itinerary.days + 1):
+                self.print_itinerary_day(day)
+        else:
+            print("Please generate the itinerary before displaying it.")
+
+    def print_itinerary_day(self, day):
+        print(f"Day {day}:")
+        self.print_category("Sightseeing Spots", self.itinerary.destination.attractions)
+        self.print_category("Restaurants", self.itinerary.destination.restaurants)
+        self.print_category("Transportation Options", self.itinerary.destination.transportation)
+        # ... print other itinerary details
+
+    def print_category(self, category_name, items):
+        print(category_name + ":")
+        for item in items:
+            print(f"- {item}")
+
+
+# Example usage
+travel_planner = AIPlanner()
+
+# Create user profile
+preferences = ["Beach", "Historical Sites"]
+interests = ["Museums", "Water Sports"]
+budget = 5000
+travel_dates = ["2022-06-01", "2022-06-07"]
+requirements = ["Wheelchair accessibility"]
+travel_planner.create_user_profile(preferences, interests, budget, travel_dates, requirements)
+
+# Add destinations
+destination1 = Destination("City A", ["Attraction 1", "Attraction 2"], ["Event 1"], ["Activity 1"], ["Local Experience 1"])
+destination2 = Destination("City B", ["Attraction 3", "Attraction 4"], ["Event 2"], ["Activity 2"], ["Local Experience 2"])
+travel_planner.add_destination(destination1)
+travel_planner.add_destination(destination2)
+
+# Generate itinerary
+travel_planner.generate_itinerary()
+
+# Display itinerary
+travel_planner.display_itinerary()
+```
+
+Here's what has been improved:
+
+1. Added a method `print_category` to print items of a specific category in the itinerary.
+2. Separated the printing of each itinerary day into a separate method `print_itinerary_day` for better readability.
+3. Removed the unnecessary comment `# ... other itinerary details` in the `display_itinerary` method.
+4. Updated the comments for the `optimize_itinerary` method to clarify that you need to implement the machine learning algorithms for itinerary optimization.
+5. Improved the formatting and organization of the code for better readability.
+6. Removed the `TODO` comment in the `optimize_itinerary` method to avoid confusion.
